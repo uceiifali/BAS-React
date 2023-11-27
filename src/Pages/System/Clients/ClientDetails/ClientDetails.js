d7cac7f073cd0861eb39a33be6b3d8f732f99acd
habeeb-nasr-12 <habeebnasr4@gmail.com>
2023-11-27 13:40:02 +0200

    .

--- src/Pages/System/Clients/ClientDetails/ClientDetails.js
+++ src/Pages/System/Clients/ClientDetails/ClientDetails.js
@@ -77,13 +77,13 @@ const ClientDetails = () => {
                 <SearchComponent background={"#161620 !important"} />
 
                 <div className='d-flex justify-content-between mt-4 w-100'>
-                    <Link to={"/System/Allclients"} className='pointer' >
+                   
                         <p className=' text-white '>
                             كل العملاء
                         </p>
 
 
-                    </Link>
+                 
                     <NavDropdown title={
                         <svg xmlns="http://www.w3.org/2000/svg" width="17" height="15" viewBox="0 0 17 15" fill="none">
                             <path d="M17 0H0L6.8 7.77417V13.1487L10.2 14.7923V7.77417L17 0Z" fill="#D59921" />
