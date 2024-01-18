977df3537b8ffad0235473abb6524178836b2342
habeeb-nasr-12 <habeebnasr4@gmail.com>
2024-01-18 21:38:51 +0200

    adding reception

--- src/Pages/System/Settings/Reception/Reception.jsx
+++ src/Pages/System/Settings/Reception/Reception.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Accordion } from "react-bootstrap";
 
 const Reception = () => {
   return (
-    <div className=''>
-      
-    </div>
-  )
-}
+    <section className="Reception">
+      <div className="grid grid-cols-4 gap-4">
+        <div className="bg-[#1E1E2D] h-[801px]  rounded-[19px]">
+          <div className="p-4 ">
+            <p className="text-white">الاستقبال</p>
+            <Accordion>
+              <Accordion.Body>
+                <div className="tabs">
+                  <div className="tab  text-[#EFAA20] ">إدارة الاستقبال</div>
+                </div>
+              </Accordion.Body>
+            </Accordion>
+          </div>
+        </div>
+        <div className="bg-[#1E1E2D] h-[801px]  rounded-[19px]">02</div>
+        <div className="col-span-2 bg-[#1E1E2D] h-[801px]  rounded-[19px]">
+          03
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Reception
+export default Reception;
