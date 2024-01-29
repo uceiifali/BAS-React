4750bb680973f9781c35571461d71e23c096f7f7
MokhtarElmahdy481 <mokhtarmahdy481@gmail.com>
2024-01-29 12:45:16 +0200

    add new feature

--- src/Pages/System/PlanModel/index.jsx
+++ src/Pages/System/PlanModel/index.jsx
@@ -1,22 +1,29 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { IoMdArrowDropright } from "react-icons/io";
 import { Box, Button, IconButton } from "@mui/material";
 import { Accordion } from "react-bootstrap";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import Style from "./PlanModel.module.css";
 import SystemControler from "../../../Components/System/SystemControler/SystemControler";
-import ProjectContextProvier, { ProjectContext } from "./Projects/ProjectContext";
+import ProjectContextProvier, {
+  ProjectContext,
+} from "./Projects/ProjectContext";
 import { DataTable } from "./Projects";
 
-const NavItem = ({ title,search }) => {
-  const {setProjects} = useContext(ProjectContext)
+const NavItem = ({ title, search }) => {
+  const { setProjects } = useContext(ProjectContext);
 
   return (
     <div className="tab p-2 my-2 rounded-md bg-[#2B2B40] text-white/50 text-end w-100">
       <button
-      onClick={()=> setProjects(DataTable?.filter(item=> item.status === search))}
-      className="w-full">{title}</button>
+        onClick={() =>
+          setProjects(DataTable?.filter((item) => item.status === search))
+        }
+        className="w-full"
+      >
+        {title}
+      </button>
     </div>
   );
 };
@@ -34,7 +41,7 @@ const CustomNav = ({ title, path, items }) => {
 
         <Accordion.Body className="my-2">
           <div className="tabs  d-flex justify-content-center align-items-center flex-column">
-            {items.map(({ title,search }, index) => (
+            {items.map(({ title, search }, index) => (
               <NavItem title={title} search={search} key={index} />
             ))}
           </div>
@@ -43,16 +50,157 @@ const CustomNav = ({ title, path, items }) => {
     </Accordion>
   );
 };
+const ModalTitle = ({ title }) => {
+  return <p className="text-[#EFAA20] text-xl font-medium">إضافة مشروع جديد</p>;
+};
+
+const FormModal = ({ children, title }) => {
+  return (
+    <fieldset className="p-3 border !border-[#d5992133]">
+      {title ? <FormTitle title={title} /> : null}
+
+      {children}
+    </fieldset>
+  );
+};
+
+const FormTitle = ({ title }) => {
+  return (
+    <legend className="text-white text-base font-medium mx-auto text-center">
+      {title}
+    </legend>
+  );
+};
+
+const InputLabel = ({ label, id }) => {
+  return (
+    <label htmlFor={id} className="text-white text-xs font-medium mb-2">
+      {label}
+    </label>
+  );
+};
+
+const AddModal = () => {
+  return (
+    <div className="p-3 border !border-[#EFAA20] rounded-[27px]">
+      <ModalTitle title={"إضافة مشروع جديد"} />
+      <div className="flex flex-col gap-4">
+        <FormModal title={"بحث عن المشروع"}>
+          <div className="grid grid-cols-12 justify-between">
+            <div className="col-span-5">
+              <InputLabel id="new-project-name" label={"اسم المشروع"} />
+              <input
+                id="new-project-name"
+                type="text"
+                className="w-full p-2 bg-[#2B2B40] rounded-[7px]"
+                placeholder="ابحث عن ...."
+              />
+            </div>
+            <div className="col-span-2"></div>
+            <div className="col-span-5">
+              <InputLabel id="find-project" label={"اسم مشروع جديد"} />
+              <input
+                id="find-project"
+                type="text"
+                className="w-full p-2 bg-[#2B2B40] rounded-[7px]"
+                placeholder="ادخل اسم المشروع الجديد"
+              />
+            </div>
+          </div>
+        </FormModal>
+        {/* <div className="flex justify-end py-3">
+          <button className="w-[140px] h-[30px] rounded-md  bg-[#EFAA20] text-[#1E1E2D] text-[15px] font-medium">
+            التالى
+          </button>
+        </div> */}
+        <FormModal title={"تفاصيل المهمة"}>
+          <div className="grid grid-cols-12 mb-5">
+            <div className="col-span-5">
+              <InputLabel id="new-project-name" label={"اسم المسؤل"} />
+              <input
+                id="new-project-name"
+                type="text"
+                className="w-full p-2 bg-[#2B2B40] rounded-[7px]"
+                placeholder="اختار اسم المشرف"
+              />
+            </div>
+            
+          </div>
+          <div className="grid grid-cols-12 mb-5">
+            <div className="col-span-5">
+              <InputLabel id="new-project-name" label={"تاريخ التسليم"} />
+              <input
+                id="new-project-name"
+                type="text"
+                className="w-full p-2 bg-[#2B2B40] rounded-[7px]"
+                placeholder="اضف تاريخ التسليم"
+              />
+            </div>
+            <div className="col-span-2"></div>
+            <div className="col-span-5">
+              <InputLabel id="find-project" label={"تاريخ الاستلام"} />
+              <input
+                id="find-project"
+                type="text"
+                className="w-full p-2 bg-[#2B2B40] rounded-[7px]"
+                placeholder="اضف تاريخ الاستلام"
+              />
+            </div>
+          </div>
+        </FormModal>
+      </div>
+    </div>
+  );
+};
 
 export default function PlanModel() {
-  const [openAddPlan, setOpenAddPlan] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [content, setContent] = useState(
+    <IconButton onClick={() => navigate("/System/plans")}>
+      <IoMdArrowDropright color="white" fontSize={25} />
+    </IconButton>
+  );
+  const { pathname } = useLocation();
+  console.log(pathname);
+  useEffect(() => {
+    if (pathname === "/System/plans/projects") {
+      setContent(
+        <button
+          className="bg-[#2B2B40] text-white rounded-md h-[38px] w-[200px] text-base "
+          onClick={() => setOpenModal(true)}
+        >
+          اضافه جديدة
+        </button>
+      );
+    } else if (pathname === "/System/plans/tasks") {
+      setContent(
+        <button
+          className="bg-[#2B2B40] text-white rounded-md h-[38px] w-[200px] text-base "
+          onClick={() => setOpenModal(true)}
+        >
+          اضافه جديدة
+        </button>
+      );
+    } else {
+      setContent();
+    }
+  }, [pathname]);
+  const navigate = useNavigate();
   return (
     <Box>
       <SystemControler
         child={
-          <IconButton>
-            <IoMdArrowDropright color="white" fontSize={25} />
-          </IconButton>
+          <>
+            <IconButton
+              onClick={() => {
+                navigate("/System/plans");
+                setOpenModal(false);
+              }}
+            >
+              <IoMdArrowDropright color="white" fontSize={25} />
+            </IconButton>
+            {content}
+          </>
         }
       />
 
@@ -64,9 +212,9 @@ export default function PlanModel() {
               title={"المشاريع"}
               path={"/System/plans/projects"}
               items={[
-                { title: "مشاريع قيد التنفيذ" , search: "قيد التنفيذ"},
-                { title: "مشاريع معلقه" , search: "معلقه"},
-                { title: "مشاريع منتهيه" , search: "منتهيه"},
+                { title: "مشاريع قيد التنفيذ", search: "قيد التنفيذ" },
+                { title: "مشاريع معلقه", search: "معلقه" },
+                { title: "مشاريع منتهيه", search: "منتهيه" },
               ]}
             />
             <CustomNav
@@ -81,8 +229,7 @@ export default function PlanModel() {
           </div>
         </div>
         <div className="p-2 col-span-3 bg-[#1E1E2D] h-[801px]  rounded-[19px]">
-          
-            <Outlet />
+          {openModal ? <AddModal /> : <Outlet />}
         </div>
       </div>
     </Box>
