ed44472fec6235e4d0bf8d39876b0b9abd19ff7f
habeeb-nasr-12 <habeebnasr4@gmail.com>
2024-02-06 15:32:08 +0200

    Finishing User Chat

--- src/Pages/System/Chat/UserChat.jsx
+++ src/Pages/System/Chat/UserChat.jsx
@@ -3,10 +3,12 @@ import { useParams } from "react-router-dom";
 import { egypetUsers, saudiaUsers } from "./Chatconsts";
 import Image from "../../../Components/Image";
 import style from "./Chat.module.css";
+import "./chatInput.css";
 import { TextField } from "@mui/material";
 import Input from "../../../Components/FormHandler/Input";
 import { IoSend } from "react-icons/io5";
-import EmojiPicker from "emoji-picker-react";
+// import EmojiPicker from "emoji-picker-react";
+import InputEmoji from "react-input-emoji";
 const UserChat = () => {
   const { country, id } = useParams();
   const [user, setUser] = useState();
@@ -26,12 +28,24 @@ const UserChat = () => {
     setUser(user[0]);
     return user[0];
   };
+  const handleEmojee = (emojes) => {
+    console.log(emojes.emoji);
+  };
+  
+  // handle input chat
+  const [text, setText] = useState("");
+  function handleOnEnter(text) {
+    console.log("text is", text);
+  }
+  const handleSendMessage = () => {
+    setText("");
+  };
   useEffect(() => {
     Getuser(country, id);
   }, [country, id]);
 
   return (
-    <div className="w-full h-full  p-2">
+    <div className="w-full h-full relative    p-2">
       <p className="text-[#D59921]  text-center ">{user?.poisition}</p>
       <div className="my-3 flex gap-3">
         <Image
@@ -71,27 +85,32 @@ const UserChat = () => {
           </div>
         </div>
       </div>
-      <div className="w-full mt-2 relative   bg-[#151521] rounded-[20px] border !border-[#414162] h-[97px] overflow-y-auto overflow-x-hidden scrollbar-none  !scrollbar-track-[transparent] !scrollbar-thin !scrollbar-thumb-[#D9D9D9]">
-        <input
-          // color="#fff"
-          // inputProps={{ style: { color: "red" } }}
-          className="form-control mb-2 overflow-x-hidden h-[50px] overflow-y-auto scrollbar-none  !scrollbar-track-[transparent] !scrollbar-thin !scrollbar-thumb-[#D9D9D9]   !bg-[#151521] !w-full  !text-start !text-white"
-          placeholder="ارسال رسالة"
-          variant="standard"
+      <div
+        className={`w-full mt-2  chatInputContainer  bg-[#151521] rounded-[20px] border !border-[#414162] h-[97px] overflow-x-hidden scrollbar-none  !scrollbar-track-[transparent] !scrollbar-thin !scrollbar-thumb-[#D9D9D9] ${style.chatInput}`}
+      >
+        <InputEmoji
+          value={text}
+          onChange={setText}
+          cleanOnEnter
+          onEnter={handleOnEnter}
+          language={"ar" | "en"}
+          keepOpened
+          shouldReturn
+          borderRadius="26"
+          borderColor="00FFFFFF"
+          inputClass=" !border-transparent   text-white text-start   bg-[#151521]"
+          placeholder="ارسل رسالة"
+          theme="dark"
         />
+        <div className="bg-[#414162] my-2 w-90 mx-auto h-[1px]"></div>
         <div className="mx-2 flex justify-between ">
-          <IoSend color="#414162" className="pointer" size={25} />
-          <div>
-            <Image
-              src="/icons/emoje.png"
-              alt="emoje icon"
-              className={"pointer  w-[20px] h-[20px] rounded-[50%]"}
-              onClick={() => {
-                setOPenEmojee(!openEmojee);
-              }}
-            />
-            <EmojiPicker open={emojeeCase} className="absolute top-1 !left-0" />
-          </div>
+          <IoSend
+            onClick={handleSendMessage}
+            color="#414162"
+            className="pointer"
+            size={25}
+          />
+          <div></div>
         </div>
       </div>
     </div>
