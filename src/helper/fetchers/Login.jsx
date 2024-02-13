a9e3d2c96d93b7d7dcbc0aeae9cdd4d55a640aba
habeeb-nasr-12 <habeebnasr4@gmail.com>
2024-02-13 23:09:36 +0200

    .

--- src/helper/fetchers/Login.jsx
+++ src/helper/fetchers/Login.jsx
@@ -1,5 +1,2 @@
 import myAxiosInstance from "../https";
-
-export const userLogin = async (userData) => {
-  return myAxiosInstance.post("/user/login", userData);
-};
+export const userLogin = (userData) => myAxiosInstance.post("/user/login", userData);
